1e269d33c58a4d3a5e8b627be8930b6710d3d8d9
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-22 23:12:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number1 = 5 ;
+        int number2 = 7 ;
+        summation(number1, number2);
+        division(number1, number2);
+
+    }
+    static void summation(int number1, int number2) {
+        System.out.println(number1 + number2);
+    }
+    static void division(int number1, int number2) {
+        System.out.println(number1 / number2);
+    }
+
+
+}
