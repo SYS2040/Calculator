5944db6bbdc37be2e665c6e539bf4415924e5cc2
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-22 23:15:07 +0200

    adding welcome msgs from master branch

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Welcome to main method");
         int number1 = 5 ;
         int number2 = 7 ;
         summation(number1, number2);
@@ -9,9 +9,11 @@ public class Main {
 
     }
     static void summation(int number1, int number2) {
+        System.out.println("Welcome to summation method");
         System.out.println(number1 + number2);
     }
     static void division(int number1, int number2) {
+        System.out.println("Welcome to division method");
         System.out.println(number1 / number2);
     }
 
