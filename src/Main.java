418b8f9992c310d9a3ce0762d4f00aba14e2b762
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-22 23:17:37 +0200

    multi welcome msges

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to main from multi");
         int number1 = 5 ;
         int number2 = 7 ;
         summation(number1, number2);
@@ -9,9 +10,11 @@ public class Main {
 
     }
     static void summation(int number1, int number2) {
+        System.out.println("Welcome to summation from multi");
         System.out.println(number1 + number2);
     }
     static void division(int number1, int number2) {
+        System.out.println("Welcome to Division from multi");
         System.out.println(number1 / number2);
     }
 
