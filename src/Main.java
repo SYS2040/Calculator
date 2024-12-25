c5f51ff1e47c3cf81dcd1a7ea921f7c32d5d58be
Mahmoud Sharaf <mahmoud.elmasry7018@gmail.com>
2024-12-26 00:26:36 +0200

    new feature added

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to main method");
 
-        System.out.println("Welcome to main from multi");
-        int number1 = 5 ;
-        int number2 = 7 ;
-        summation(number1, number2);
-        division(number1, number2);
 
     }
+
+
     public static void bFeature (){
         System.out.println("hello from feature b");
     }
@@ -21,9 +17,8 @@ public class Main {
         System.out.println(number1 + number2);
     }
     static void division(int number1, int number2) {
-        System.out.println("Welcome to division method");
-        System.out.println("Welcome to Division from multi");
-        System.out.println(number1 / number2);
+
+        System.out.println("Result : " +number1 / number2);
     }
 
 
